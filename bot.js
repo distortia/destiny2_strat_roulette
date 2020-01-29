e46840108ff15a565ad475802bdb9c77423cb81f
shibuyamizou <nickstalter@gmail.com>
2020-01-29 17:41:27 -0500

    Add simple nodejs server

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+const http = require('http')
+
+const port = process.env.PORT || 3000
+
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const GDocs = require('google-spreadsheet')
@@ -5,35 +9,45 @@ const GDocs = require('google-spreadsheet')
 let doc = new GDocs('1hVzkJKOJBL5Kd5YKA-KUxo1cdpWpQK06pLhR5uA0ar8')
 const TOKEN = process.env.TOKEN
 
-bot.login(TOKEN)
-
-let randomNumber = (max) => {
-    return Math.floor(Math.random() * Math.floor(max))
-}
-
-bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`)
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World')
 })
 
-bot.on('message', msg => {
-    if (msg.content === '!strat') {
-        doc.getInfo((_err, info) => {
-            sheet = info.worksheets[0]
-            sheet.getRows({
-                offset: 1,
-            }, (err, rows) => {
-                if (err) {
-                    console.dir(err)
-                }
-                random_row = randomNumber(rows.length)
-                row = rows[random_row]
-                strat = {
-                    name: row.name,
-                    description: row.description,
-                    creator: row.creator
-                }
-                msg.reply(`#${random_row} **${strat.name}** - ${strat.description} by *${strat.creator}*`)
-            })
-        })
+server.listen(port, () => {
+    console.log('server up')
+    bot.login(TOKEN)
+    
+    let randomNumber = (max) => {
+        return Math.floor(Math.random() * Math.floor(max))
     }
+    
+    bot.on('ready', () => {
+        console.info(`Logged in as ${bot.user.tag}!`)
+    })
+    
+    bot.on('message', msg => {
+        if (msg.content === '!strat') {
+            doc.getInfo((_err, info) => {
+                sheet = info.worksheets[0]
+                sheet.getRows({
+                    offset: 1,
+                }, (err, rows) => {
+                    if (err) {
+                        console.dir(err)
+                    }
+                    random_row = randomNumber(rows.length)
+                    row = rows[random_row]
+                    strat = {
+                        name: row.name,
+                        description: row.description,
+                        creator: row.creator
+                    }
+                    msg.reply(`#${random_row} **${strat.name}** - ${strat.description} by *${strat.creator}*`)
+                })
+            })
+        }
+    })
 })
+
